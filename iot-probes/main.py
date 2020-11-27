b985a549fa759bca6298004eb615ef2e939073f1
Steven Pilger <stevenpilger[at]fastmail[dot]com>
2020-11-27 22:21:39 +0100

    fix: post request issue
    switch to socket implementation to issue post request, dummy data works now!

--- iot-probes/main.py
+++ iot-probes/main.py
@@ -1,9 +1,11 @@
+import usocket as socket
 import machine
 from machine import Pin
 import dht
 import network
-import time
+from time import sleep
 import urequests as requests
+from random import randrange
 
 def read_secrets(credential_path):
     credentials = {}
@@ -24,11 +26,36 @@ def connect_wlan(SSID, PW):
             pass
     print('Network Config:', wlan.ifconfig())
 
+
+def request(ip, port, endpoint, request_type = 'GET', data = {}, use_stream=False):
+    s = socket.socket()
+
+    ai = socket.getaddrinfo(ip, port)
+    addr = ai[0][-1]
+    query_parameters = parse_data_to_query_params(data)
+    url = '/{}{}'.format(endpoint, query_parameters)
+
+    print("Connect address:", addr)
+    s.connect(addr)
+    s.send(b"{} {} HTTP/1.0\r\n\r\n".format(request_type, url))
+
+    s.close()
+
+def parse_data_to_query_params(data = {}):
+    query_params = ''
+    for i, element in enumerate(data.items()):
+        prefix = '&' if i != 0 else '?'
+        param_element = prefix + '='.join([str(element[0]), str(element[1])])
+        query_params += param_element
+    return query_params
+
+
+
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
     print('woke from a deep sleep')
 
 # Sleep Specification
-sleep_duration = 1
+sleep_duration = 10
 seconds_per_minute = 60
 seconds_to_miliseconds_factor = 1000
 deepsleep_length = sleep_duration * seconds_per_minute * seconds_to_miliseconds_factor
@@ -37,7 +64,7 @@ deepsleep_length = sleep_duration * seconds_per_minute * seconds_to_miliseconds_
 device_config = read_secrets('./device.secret')
 
 # Wait for 2 seconds upon boot
-time.sleep(2)
+sleep(2)
 
 #Connect to WLAN
 credentials = read_secrets('./wifi.secret')
@@ -52,12 +79,28 @@ connect_wlan(credentials['SSID'], credentials['PASSWORD'])
 # Take analog soil moisture measurement
 # add code here
 
-# Post data to collector
-api_config = read_secrets('./nodered-api.secret')
-url = 'http://{}:{}/{}'.format(api_config['LANIP'], api_config['PORT'], api_config['ENDPOINT'])
-url = url + '?temperature={}&humidity={}&device={}'.format(12.345, 45.678, device_config['NAME'])
+# build data object for post request
+temp = randrange(18,35)
+humid = randrange(1,100)
+data = {
+    'temperature': temp,
+    'humidity': humid,
+    'device': device_config['NAME']
+}
+p = Pin(23, Pin.OUT)
+p.on()
 
-response = requests.post(url)
+api_config = read_secrets('./nodered-api.secret')
+request(
+    ip = api_config['LANIP'],
+    port = api_config['PORT'],
+    endpoint = api_config['ENDPOINT'],
+    request_type = 'POST',
+    data = data
+)
+
+p.off()
+sleep(1)
 
 # Go to deepsleep
-machine.deepsleep(10000)
+machine.deepsleep(deepsleep_length)
