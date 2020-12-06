e0df7f25727171225ff5b20f54e2ddd3f6279838
Steven Pilger <stevenpilger[at]fastmail[dot]com>
2020-12-06 01:27:19 +0100

    feat: add keyboard emoji as favicon

--- frontend/frontend/pages/_app.js
+++ frontend/frontend/pages/_app.js
@@ -13,6 +13,11 @@ function MyApp({ Component, pageProps, preview }) {
     <Layout preview={preview}>
       <Head>
         <title>stevenpilger.com</title>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2250%22 fill=%22%23d2c3a2%22></rect><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2280%22>⌨️</text></svg>"
+        />
       </Head>
 
       <Header open={open} setOpen={setOpen} />
